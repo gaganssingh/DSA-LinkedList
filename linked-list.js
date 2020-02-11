f58669df5a496aeb13b1532b4be047e82b0c56a1
Gagan Singh <gagansingh.dev@outlook.com>
2020-02-11 10:49:30 -0500

    Added drills

--- linked-list.js
+++ linked-list.js
@@ -74,4 +74,106 @@ class LinkedList {
         }
         previousNode.next = currNode.next;
     }
+
+    insertBefore(newItem, nextItem) {
+        let currentNode = this.head;
+
+        while (currentNode.next !== null) {
+            if (currentNode.next.value == nextItem) {
+                let newNode = new _Node(newItem);
+                newNode.next = currentNode.next;
+                currentNode.next = newNode;
+                return;
+            }
+            currentNode = currentNode.next;
+        }
+        this.size++;
+    }
+
+    insertAfter(newItem, currentItem) {
+        let currentNode = this.head;
+
+        while (currentNode.value !== null) {
+            if (currentNode.value == currentItem) {
+                let newNode = new _Node(newItem);
+
+                newNode.next = currentNode.next;
+
+                currentNode.next = newNode;
+                return;
+            }
+            currentNode = currentNode.next;
+        }
+        this.size++;
+    }
+
+    insertAt(newItem, i) {
+        if (i > this.size) {
+            return;
+        }
+
+        if (i === 0) {
+            this.insertFirst(newItem);
+            return;
+        }
+
+        const newNode = new _Node(newItem);
+        let currentNode, previousNode;
+        currentNode = this.head;
+        let count = 0;
+        while (count < i) {
+            previousNode = currentNode;
+            count++;
+            currentNode = currentNode.next;
+            insert;
+        }
+
+        newNode.next = currentNode;
+        previousNode.next = newNode;
+
+        this.size++;
+    }
 }
+
+// ======DRILLS======
+// Create a singly linked list
+// & Supplemental functions
+function main(arr) {
+    const SLL = new LinkedList();
+    arr.forEach((name) => SLL.insertLast(name));
+    SLL.insertLast("Tauhida"); // Add Tauhida
+    SLL.remove("Husker"); // Remove Husker
+    // SLL.insertBefore("ItemToInsertBefore", "Husker");
+    // SLL.insertAfter("ItemToInsertAfter", "Husker");
+    // SLL.insertAt("ItemToInsertAt", 2);
+    SLL.insertBefore("Athena", "Boomer"); // Add Athena before Boomer
+    SLL.insertAfter("HotDog", "Helo");
+    SLL.insertAt("Kat", 3);
+    SLL.remove("Tauhida"); // Remove Tauhida
+}
+// main(["Apollo", "Boomer", "Helo", "Husker", "Starbuck"]);
+
+// display: displays the linked list
+// size: returns the size of the linked list
+// isEmpty: finds if the list is empty or not (without using the size() function)
+// findPrevious: finds the node before the item you are looking for
+// findLast: returns the last node in the linked list
+
+// Mystery program
+// Analyze the following function (without running it in an IDE) to determine what problem it is trying to solve. What is the time complexity of this algorithm?
+
+// function WhatDoesThisProgramDo(lst) {
+//     let current = lst.head;
+//     while (current !== null) {
+//         let newNode = current;
+//         while (newNode.next !== null) {
+//             if (newNode.next.value === current.value) {
+//                 newNode.next = newNode.next.next;
+//             }
+//             else {
+//                 newNode = newNode.next;
+//             }
+//         }
+//         current = current.next;
+//     }
+// }
